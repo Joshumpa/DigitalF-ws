1eca0f547db5e3a05b73649ec96aa72be332bd5c
Joshua Marinez <joshua.marinez1@gmail.com>
2020-10-19 13:53:54 -0700

    implementacion de ms sql

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
-const randomInt = require('random-int');
-const server = require('http').createServer();
+var express = require('express');
+var app = express();
 
-const io = require('socket.io')(server, {
+var sql = require('mssql');
+
+sql.connect("mssql://spark:spark@MXL30INBOWHD7Y2/SparkDB-IND").then(function () {
+    // Query 
+    new sql.Request().query('select * from HydraDataL3_Catalog').then(function (recordset) {
+        console.dir(recordset);
+    }).catch(function (err) {
+        console.dir("Error, Query no estructurada correctamente")
+    });
+});
+
+
+/*const io = require('socket.io')(server, {
     transports: ['websocket', 'polling']
 });
 
-let tick = 0;
-// 1. listen for socket connections
+
 io.on('connection', client => {
     setInterval(() => {
         client.emit('rnddata', data = [
@@ -19,18 +30,18 @@ io.on('connection', client => {
                 nombr: "Maquina 2",
                 d1: randomInt(0, 100),
                 d2: randomInt(0, 100),
-            },{
+            }, {
                 nombr: "Maquina 3",
                 d1: randomInt(0, 100),
                 d2: randomInt(0, 100),
-            },{
+            }, {
                 nombr: "Maquina 4",
                 d1: randomInt(0, 100),
                 d2: randomInt(0, 100),
             }
 
         ]);
-}, 2000);
+    }, 2000);
 });
 
-server.listen(3000);
+server.listen(3000);*/
