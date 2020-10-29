997a13ef035b31488be059c3b7160deceb5083a9
Joshua Marinez <joshua.marinez1@gmail.com>
2020-10-29 06:53:51 -0700

    un poco de bootstrap, se arreglaron fallas al envio de datos del servidor pero aún hay algunas

--- src/client.js
+++ src/client.js
@@ -2,103 +2,200 @@ import io from 'socket.io-client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useEffect, useState } from 'react';
-//import createGauge from './scripts';
-let JustGauge = require('justgage');
-
-const createGauge = (properties) => {
-
-    //id = ariable, value, title = variable, label = measure, max, min, not- satisfactory
-    let gauge = new JustGage({
-        id: properties.Variable,
-        value: properties.Value,
-        width: 520,
-        height: 520,
-        min: 0,
-        max: properties.Max,
-        humanFriendlyDecimal: 0,
-        title: properties.Variable,
-        label: properties.Measure,
-        pointer: true,
-        pointerOptions: {
-            toplength: -13,
-            bottomlength: 10,
-            bottomwidth: 12,
-            color: '#8e8e93',
-            stroke: '#ffffff',
-            stroke_width: 3,
-            stroke_linecap: 'round'
-        },
-        customSectors: {
-            ranges: [{
-                color: '#00CC22',
-                lo: properties.Min,
-                hi: properties.Max
-            },
-            {
-                color: '#E6E600',
-                lo: properties.Min,
-                hi: properties.NotSat,
-            },
-            {
-                color: '#E60000',
-                lo: properties.NotSat,
-                hi: 0,
-            }]
-        },
-        counter: true,
-        gaugeWidthScale: 0.75,
-        formatNumber: true,
-        hideInnerShadow: true
-    });
-
-    return gauge
-
-}
+import JustGauge from 'justgage';
+//import '../node_modules/react-canvas-gauges/dist/RadialGauge.js';
+//import RadialGauge from 'canvas-gauges'
 
 const socket = io('http://localhost:3000', {
     transports: ['websocket', 'polling']
 });
 
-
 function App() {
 
-    let [data, setData] = useState([]);
-
+    const [data, setData] = useState([])
+    let value = 0;
+    //const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
-        let isMounted = true;
+        //setIsLoading(true)
         socket.on('infoHydra', data => {
-
-            let gauges = {};
-            if (isMounted) setData(data);
-            data.forEach(properties => {
-                gauges.push(createGauge(properties));
-            });
+            setData(data)
         })
-        return () => { isMounted = false }; // use effect cleanup to set flag false, if unmounted
     });
 
-    console.log(data);
+    //console.log(data);
 
-    if (typeof data[0] !== 'undefined') {
-
-
-
-        //gg1.refresh(cycle.Value);
+    if (data[0] !== undefined) {
+        //console.log(value);
+        gg1.refresh(data.find(obj => obj.Variable == "Cycle").Value);
+        gg2.refresh(data.find(obj => obj.Variable == "InjTime").Value);
+        gg3.refresh(data.find(obj => obj.Variable == "RecovTime").Value);
+        gg4.refresh(data.find(obj => obj.Variable == "MCushion").Value);
+        gg5.refresh(data.find(obj => obj.Variable == "PeakPrs").Value);
     }
 
+
     return (
-
         <div>
-
-            {/* <ReactTable
-                data={data}
-                columns={columns}
-            /> */}
-
+            {/* <RadialGauge
+            units='Sec'
+            title='Cycle'
+            value={value}
+            minValue={0}
+            maxValue={50}
+            majorTicks={['0', '5', '15', '20', '25', '30', '35', '40', '45', '50']}
+            minorTicks={2}
+        ></RadialGauge> */}
         </div>
-
     );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//id = ariable, value, title = variable, label = measure, max, min, not- satisfactory
+let dflt = {
+    min: 0,
+    humanFriendlyDecimal: 1,
+    decimals: 2,
+    pointer: true,
+    pointerOptions: {
+        toplength: -20,
+        bottomlength: 10,
+        bottomwidth: 20,
+        color: '#8e8e93',
+        stroke: '#ffffff',
+        stroke_width: 5,
+        stroke_linecap: 'round'
+    },
+    counter: false,
+    gaugeWidthScale: 0.75,
+    formatNumber: true,
+    hideInnerShadow: true
+
+}
+
+let gg1 = new JustGauge({
+    id: "gg1",
+    value: 22,
+    max: 25,
+    title: "Cycle",
+    label: "sec",
+    defaults: dflt,
+    customSectors: {
+        ranges: [{
+            color: '#00CC22',
+            lo: 10,
+            hi: 25
+        },
+        {
+            color: '#E6E600',
+            lo: 1,
+            hi: 10,
+        },
+        {
+            color: '#E60000',
+            lo: 1,
+            hi: 0,
+        }]
+    }
+});
+let gg2 = new JustGauge({
+    id: "gg2",
+    value: 0.40,
+    max: 0.80,
+    defaults: dflt,
+    title: "Inyection Time",
+    label: "sec",
+    customSectors: {
+        ranges: [{
+            color: '#00CC22',
+            lo: 0.20,
+            hi: 0.80
+        },
+        {
+            color: '#E6E600',
+            lo: 0.15,
+            hi: 0.20,
+        },
+        {
+            color: '#E60000',
+            lo: 0.15,
+            hi: 0,
+        }]
+    }
+});
+let gg3 = new JustGauge({
+    id: "gg3",
+    value: 1.50,
+    max: 2.0,
+    defaults: dflt,
+    title: "Recovery Time",
+    label: "sec",
+    customSectors: {
+        ranges: [{
+            color: '#00CC22',
+            lo: 1.0,
+            hi: 2.0
+        },
+        {
+            color: '#E6E600',
+            lo: 0.5,
+            hi: 1,
+        },
+        {
+            color: '#E60000',
+            lo: 0.5,
+            hi: 0,
+        }]
+    }
+});
+let gg4 = new JustGauge({
+    id: "gg4",
+    value: 0.70,
+    max: 1.00,
+    defaults: dflt,
+    title: "Cushion",
+    label: "in",
+    customSectors: {
+        ranges: [{
+            color: '#00CC22',
+            lo: 0.50,
+            hi: 1.00
+        },
+        {
+            color: '#E6E600',
+            lo: 0.25,
+            hi: .50
+        },
+        {
+            color: '#E60000',
+            lo: 0.5,
+            hi: 0,
+        }]
+    }
+});
+let gg5 = new JustGauge({
+    id: "gg5",
+    value: 500,
+    max: 800,
+    defaults: dflt,
+    title: "Peak Presure",
+    label: "psi",
+    customSectors: {
+        ranges: [{
+            color: '#00CC22',
+            lo: 300,
+            hi: 800
+        },
+        {
+            color: '#E6E600',
+            lo: 299,
+            hi: 300
+        },
+        {
+            color: '#E60000',
+            lo: 299,
+            hi: 0,
+        }]
+    }
+}); 
--- src/server.js
+++ src/server.js
@@ -13,30 +13,27 @@ const io = require('socket.io')(server, {
     transports: ['websocket', 'polling']
 });
 
-let c=0;
+let c = 0;
 //cuando haya una conexion con el servidor...
 io.on('connection', client => {
 
     //console.dir("Nuevo cliente conectado")
 
-    let status = true;
+    let status = true
 
     //realizar funcion con un intervalo de 2000 milisegundos
     setInterval(() => {
 
         if (status) {
 
-            status = false;
-
+            status = false
             //query que selecciona los primeros renglones más recientes según cada valor distinto en la columna "Variable"
-
             new sql.Request().query(`
             
-                SELECT TOP 6 Time, A.Machine, A.Variable, A.Value, Target, B.Max, B.Min, Measure, NotSat 
-                FROM HydraDataL3 AS A LEFT JOIN HydraDataL3_Catalog AS B
-                ON A.Variable=B.Variable AND A.Machine=B.Machine 
-                WHERE Time LIKE(SELECT TOP 1 MAX(Time)AS Time FROM HydraDataL3)
-                AND A.Variable in('Cycle','Good','PeakPrs','Mcushion','InjTime','Recovtime')
+                SELECT TOP 6 *
+                FROM HydraDataL3
+                WHERE Variable in('Cycle','Good','PeakPrs','Mcushion','InjTime','Recovtime')
+                ORDER BY Time DESC
             
             `, (err, result) => {
                 //funcion con dos parametros, en caso de error o devolver resultado
@@ -46,16 +43,18 @@ io.on('connection', client => {
 
                 //el objeto resulto es un objeto de con 4 campos, 0-estructura, 1-datos, 2-tamaño, 3-celdas afectadas
                 let data = Object.values(result)[1];
+                //console.log(data[0].Value);
 
                 //emitir el nombre del evento, listener
                 client.emit('infoHydra', data);
 
-                status = true;
+                status = true
 
             });
-
         }
-    }, 7000);
+
+
+    }, 5000);
 });
 
 //en el puerto
